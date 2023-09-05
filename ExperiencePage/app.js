49041209e9f87762547569deafa886144bbe4b27
JT <kappstack@gmail.com>
2023-09-05 15:12:32 -0400

    Updated experience page

--- ExperiencePage/app.js
+++ ExperiencePage/app.js
@@ -1,192 +1,187 @@
-//script for managing the second scene with its entirely different camera, renderer, etc
 import * as THREE from 'https://cdn.skypack.dev/three@0.128.0';
-import * as TWEEN from 'https://cdnjs.cloudflare.com/ajax/libs/tween.js/18.6.4/tween.esm.js';
+import { EffectComposer } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/postprocessing/UnrealBloomPass.js';
 
-//make all necessary variables
-var scene = null, camera = null, renderer = null;
+// get canvas object where the scene will be drawn
+var canvasContainer = document.getElementById("canvasContainer");
+var canvas = document.getElementById('bg');
 
-//mouse trail variables
-const raycaster = new THREE.Raycaster();
-const mouse = new THREE.Vector2();
-const mouseDirection = new THREE.Vector2();
-var mouseTrailSpawnPoint = new THREE.Vector3();
+// Create variables for mouse targeting
+var mouseX = 0;
+var mouseY = 0;
+var target = new THREE.Vector3();
 
-//for mobile direction variable
-var previousTouch;
-//load mouse trail texture
-var mouseTrailTexture = new THREE.TextureLoader().load("./CustomImages/UnityDefaultParticleSprite.png" );
+// make all necessary scene variables
+var scene = null, camera = null, renderer = null, ambientLight = null;
 
-function onMouseMove( event ) {
+// make all post processing scene variables
+var renderPass = null, bloomPass = null, composer = null;
 
-	// calculate mouse position in normalized device coordinates
-	// (-1 to +1) for both components
+// make main object variables
+var geometry = null, material = null, mesh = null, widthSegments = 600, heightSegments = 600;
 
-	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+function initializeScene() {	
 
-	mouseDirection.x = event.movementX;
-	mouseDirection.y = event.movementY;
-
-	setCursorTrail();
-}
-
-function onMobileTouch( event )
-{	
-	const touch = event.touches[0];
-
-	mouse.x = ( touch.clientX / window.innerWidth ) * 2 - 1;
-	mouse.y = - ( touch.clientY / window.innerHeight ) * 2 + 1;
-
-	//calculate direction of touch from previous point as touches dont have the .movementX/Y 
-	if (previousTouch) {
-        // be aware that these only store the movement of the first touch in the touches array
-        event.movementX = touch.pageX - previousTouch.pageX;
-        event.movementY = touch.pageY - previousTouch.pageY;
-    };
-
-    previousTouch = touch;
-
-	mouseDirection.x = event.movementX;
-	mouseDirection.y = event.movementY;
-
-	console.log(event.movementY)
-
-	setCursorTrail();
-}
-
-function setCursorTrail()
-{	
-	const geometry = new THREE.PlaneGeometry( 0.003, 0.003 );
-	const material = new THREE.MeshBasicMaterial( {map: mouseTrailTexture, transparent: true, opacity: 1,} );
-	material.color.set(0xffffff)
-	var plane = new THREE.Mesh( geometry, material );
-	plane.position.x = mouseTrailSpawnPoint.x;
-	plane.position.y = mouseTrailSpawnPoint.y;
-	scene.add( plane );
-
-	//the direction the - plane.position.clone().setX(Math.random() < 0.5 ? -10 : 10).setY(Math.random() < 0.5 ? -10 : 10)
-	let planeEndingPos = plane.position.clone().setX(mouse.x - mouseDirection.x).setY(mouse.y - mouseDirection.y);
-
-	new TWEEN.Tween(plane.position)
-    .to(planeEndingPos, 20000)
-    .onStart(function() {
-      	new TWEEN.Tween(plane.material)
-		.to({color: new THREE.Color( 0xffd700 )}, 1000)
-		.onStart(function() {
-		
-		})
-		.onComplete(function() {
-			plane.geometry.dispose(); 
-			plane.material.dispose();
-			scene.remove( plane ); 
-			plane = undefined;
-		})
-		.start();
-    })
-    .onComplete(function() {
-		
-    })
-    .start();
-}
-
-function setUpPlaneForMouseTrail()
-{	
-	const geometry = new THREE.PlaneGeometry( 200, 100 );
-	const material = new THREE.MeshBasicMaterial( {color: 0x000000, side: THREE.DoubleSide} );
-	const plane = new THREE.Mesh( geometry, material );
-	scene.add( plane );
-}
-
-function initScene()
-{	
 	scene = new THREE.Scene();
-	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+	camera = new THREE.PerspectiveCamera( 75, canvasContainer.offsetWidth / canvasContainer.offsetHeight, 0.1, 1000 );
 	renderer = new THREE.WebGLRenderer({ 
-		canvas: document.querySelector('#bg'),
+		canvas: canvas,
 	});
 
 	//set positions and widths
 	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild(renderer.domElement); //this is to overlay html onto the camera
+	renderer.setSize( canvasContainer.offsetWidth, canvasContainer.offsetHeight );
 
-	//add light
-	const light = new THREE.AmbientLight( 0x404040 ); // soft white light
-	scene.add( light );
-
-	//set positions and widths
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-
-	//controls.update() must be called after any manual changes to the camera's transform
-	camera.position.set( 0, 0, 1 );
-
-	setUpPlaneForMouseTrail();
-
-	//render - draw
-	renderer.render( scene, camera );
 }
 
-//pretty much the Update() function like in unity - callen every frame
-function Update()
-{	
-    requestAnimationFrame( Update );
+function applyPostProcessing() {
 
-	// update the picking ray with the camera and mouse position
-	raycaster.setFromCamera( mouse, camera );
+	// Create a render pass for the initial scene
+	renderPass = new RenderPass(scene, camera);
 
-	// calculate objects intersecting the picking ray
-	const intersects = raycaster.intersectObjects( scene.children );
+	// Create a bloom pass
+	bloomPass = new UnrealBloomPass(
+		new THREE.Vector2(canvasContainer.offsetHeight, canvasContainer.offsetWidth),
+		0., 0.2, 0.1
+	);
 
-	for ( let i = 0; i < intersects.length; i ++ ) {
+	// Set up the composer
+	composer = new EffectComposer(renderer);
+	composer.addPass(renderPass);
+	composer.addPass(bloomPass);
 
-		mouseTrailSpawnPoint = intersects[ i ].point;
-
-	}
-
-	//update TWEEN - animation module
-	TWEEN.update();
-
-    if (renderer) renderer.render( scene, camera );
 }
 
-//init scene
-initScene();
-Update();
+function createLight() {
 
-//now before we add the event listeners, we have to do something really important
-//since the canvas will naturally be at the bottom of other elements
-//because it is edited in this script, we must append everything else
-//back to the bottom so that the canvas is at the top
-var otherElements = Array.prototype.slice.call(document.getElementsByClassName("project-section"));
-for (let i = 0; i < otherElements.length; i++) {
-	console.log(otherElements);
-	document.body.append(otherElements[i]);
+	//create light
+	ambientLight = new THREE.AmbientLight(0xfffffff);
+	scene.add(ambientLight);
+
+
 }
 
-//add event listener for mouse trail - for keyboard and mouse users
-window.addEventListener( 'mousemove', onMouseMove, false );
+function placeCamera() {
 
-//detect mobile touch move
-window.addEventListener( 'touchmove', onMobileTouch, false );
+	//set camera position
+	camera.position.set(0,0,5.5);
 
-window.addEventListener("resize", onWindowResize, false);
+}
 
-//resize the camera everytime the window is resized
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
+function createSoapyBubblePiece() {
+
+	// Create a plane geometry to apply the Perlin noise texture to
+    geometry = new THREE.BoxGeometry( 5, 5, 0.3, 650, 650 ); 
+    material = new THREE.ShaderMaterial({
+		vertexShader: `
+			uniform float time;
+			uniform vec2 resolution;
+			uniform vec2 mouse;
+			
+			varying vec3 vertexColor; 
+
+			const int AMOUNT = 50;
+			const float color_intensity = 1.0;
+			const float fluid_speed = 100.0;
+			
+			void main() {
+				
+				// Generate Color
+				vec2 coord = 0.5 * (position.xy - resolution / min(resolution.y, resolution.x));
+
+				float len;
+
+				for (int i = 1; i < AMOUNT; i++){
+					vec2 newCoord = coord + time * 0.001;
+					newCoord.x += 0.6/float(i) * sin(float(i) * coord.y + time / fluid_speed + 20.3 * float(i)) + 0.5 + mouse.y;
+					newCoord.y += 0.6/float(i) * sin(float(i) * coord.x + time / fluid_speed + 0.3 * float(i + 10)) - 0.5 + mouse.x;
+
+					coord = newCoord;
+				}
+
+				//Calculate the pattern effect within the gradient colors
+				vertexColor = vec3(color_intensity*sin(4.0*coord.x)+color_intensity, color_intensity*sin(2.0*coord.y)+color_intensity, color_intensity*sin(coord.x+coord.y)+color_intensity);
+
+				gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+
+			}
+		`,
+		fragmentShader: `
+			varying vec3 vertexColor; // Receive the color from the vertex shader
+
+			void main() {
+				gl_FragColor = vec4(vertexColor, 1.0); // Use the color passed from the vertex shader
+			}
+		`,
+		uniforms: {
+			time: { value: 0 },
+			resolution: { value: new THREE.Vector2(widthSegments, heightSegments) },
+		}
+    });
+
+	mesh = new THREE.Mesh(geometry, material);
+	scene.add(mesh);
+
+}
+
+// credit to https://stackoverflow.com/questions/53887057/threejs-object-look-at-mouse-with-ease
+
+function planeLookAtCursor() {
+
+    // Calculate the target position for the plane to look at
+    target.x += ( mouseX - (target.x * 0.2) ) * .09;
+    target.y += ( mouseY - (target.y * 0.2) ) * .09;
+    target.z = camera.position.z; // assuming the camera is located at ( 0, 0, z );
+
+	mesh.lookAt( target );
+
+}
+
+// Update function called every frame for animation
+
+function animate() {
+
+    requestAnimationFrame(animate);
+	
+	// Update time uniform for the shader
+	material.uniforms.time.value += 0.01;
+
+	// Make plane look at cursor
+	planeLookAtCursor();
+
+	// Your animation/rendering code goes here
+	if (composer) composer.render();
+} 
+
+// Mousemove event handler
+function onDocumentMouseMove(event) {
+    // Calculate the normalized mouse position in the canvas
+    mouseX = (event.clientX / window.innerWidth) * 2 - 1;
+    mouseY = - (event.clientY / window.innerHeight) * 2 + 1;
+}
+
+// Initialize three.js scene, objects, and effects
+initializeScene();
+applyPostProcessing();
+
+createLight();
+placeCamera();
+
+createSoapyBubblePiece();
+
+animate();
+
+//add event listener to update mouseX and mouseY so the piece can look at cursor
+window.addEventListener('mousemove', onDocumentMouseMove);
+
+//add event listeners to resize camera and scene, as well as the background plane
+//also add functionality to load different pages haha
+window.onresize = function onWindowResize() {
+
+	// Update Camera aspect
+    camera.aspect = canvasContainer.offsetWidth / canvasContainer.offsetHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(canvasContainer.offsetWidth, canvasContainer.offsetHeight);
+
 }
-
-//apply - load new page function
-var letsGoBackButton = document.getElementById("goBackButton");
-
-if (letsGoBackButton) letsGoBackButton.addEventListener(('click'), () =>
-{   
-    //load second page html
-    var a = document.createElement('a');
-
-    a.href = "https://johnathantam.github.io/Portfolio/SocialPage/index.html";
-    a.click();
-})
